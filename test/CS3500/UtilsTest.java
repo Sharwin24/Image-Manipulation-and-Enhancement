680d61c130dd92d8a27e966aff9e98a642125613
amburke815 <burke.ad@northeastern.edu>
2021-06-07 15:06:11 -0500

    added junit

--- test/CS3500/UtilsTest.java
+++ test/CS3500/UtilsTest.java
@@ -1,5 +1,14 @@
 package CS3500;
+import static org.junit.Assert.assertEquals;
 
+/**
+ * Tests for methods in the {@link Utils} class.
+ */
 public class UtilsTest {
+
+  /**
+   * Tests for the {@link Utils#checkNotNull(Object, String)} method.
+   */
+
 
 }
