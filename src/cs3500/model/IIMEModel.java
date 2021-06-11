d329293c5f29c8e109fe381b8c7c51e86a179835
Sharwin Patil <sharwin24@gmail.com>
2021-06-11 16:19:35 -0700

    Update IIMEModel.java

--- src/cs3500/model/IIMEModel.java
+++ src/cs3500/model/IIMEModel.java
@@ -5,25 +5,50 @@ import cs3500.model.fileFormat.IFileFormat;
 import cs3500.model.operation.IOperation;
 
 /**
- * : JavaDoc comments
+ * An interface for an Image Manipulation and Enhancement Model.
  *
- * @param <Z>
+ * @param <Z> the implementation of an image.
  */
 public interface IIMEModel<Z> {
 
   /**
+   * Applies one or more operations to the current image, in the order given to the method.
    *
-   * @throws IllegalArgumentException
+   * @param operations the operations to apply, in order of application.
+   * @throws IllegalArgumentException if any of the operations are invalid or null.
    */
   void applyOperations(IOperation... operations) throws IllegalArgumentException;
 
+  /**
+   * Imports a file and sets the current image to the imported image.
+   *
+   * @param format   the filetype of the image.
+   * @param fileName the name of the file for the image.
+   * @throws IllegalArgumentException if the file format is invalid or null.
+   */
   void importImage(IFileFormat format, String fileName)
       throws IllegalArgumentException;
 
+  /**
+   * Exports the current image to a given destination.
+   *
+   * @param format   the filetype of the exported image.
+   * @param fileName the name of the destination path.
+   * @throws IllegalArgumentException if the file format is invalid or null.
+   */
   void exportImage(IFileFormat format, String fileName)
       throws IllegalArgumentException;
 
+  /**
+   * Sets the current image to a programmatic image with the given parameters.
+   *
+   * @param imgToSet   the type of programmatic image to set to.
+   * @param widthPx    the width of the image.
+   * @param heightPx   the height of the image.
+   * @param unitSizePx the unit size of the image.
+   * @throws IllegalArgumentException if the progammatic image type is invalid or null.
+   */
   void setProgrammaticImage(IProgramImage imgToSet, int widthPx, int heightPx,
       int unitSizePx)
-  throws IllegalArgumentException;
+      throws IllegalArgumentException;
 }
