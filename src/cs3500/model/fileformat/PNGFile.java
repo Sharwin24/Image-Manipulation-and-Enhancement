8f34626eb759e95068d5f430de6632e124f2f3e3
amburke815 <burke.ad@northeastern.edu>
2021-06-18 15:32:56 -0500

    added comments to file format

--- src/cs3500/model/fileformat/PNGFile.java
+++ src/cs3500/model/fileformat/PNGFile.java
@@ -1,8 +1,9 @@
 package cs3500.model.fileformat;
 
 /**
- * A class to represent a PNG image. Offers the capability to import/export an image of * this
- * type.
+ * A class to represent a PNG image. Offers the capability to to import/export an image of this
+ * type. This class is a function object that can <code>export: IImage, String -> File</code> and
+ * <code>Import: String -> IImage</code>, and has a unique file extension ".png"
  */
 public class PNGFile extends AFileFormat {
 
