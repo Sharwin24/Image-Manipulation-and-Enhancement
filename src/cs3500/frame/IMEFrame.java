69e8b557d4112cf6bdcef2edd2acd5119b812fcd
Sharwin Patil <sharwin24@gmail.com>
2021-06-21 12:19:24 -0700

    Created a frame class and package

---
+++ src/cs3500/frame/IMEFrame.java
@@ -0,0 +1,28 @@
+package cs3500.frame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import javax.swing.JFrame;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class IMEFrame extends JFrame implements ActionListener, ItemListener,
+    ListSelectionListener {
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+
+  }
+
+  @Override
+  public void itemStateChanged(ItemEvent e) {
+
+  }
+
+  @Override
+  public void valueChanged(ListSelectionEvent e) {
+
+  }
+}
