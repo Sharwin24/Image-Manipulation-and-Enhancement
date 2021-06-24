cf92f1c5b1703b1f3df3d8e91f26aecbef9deb87
Sharwin Patil <sharwin24@gmail.com>
2021-06-24 01:07:13 -0700

    Finished Downscale

--- src/cs3500/model/operation/Downscale.java
+++ src/cs3500/model/operation/Downscale.java
@@ -21,21 +21,47 @@ public class Downscale {
   private int newHeight;
   private int newWidth;
 
-  public Downscale(IMultiLayerModel model, int newHeight, int newWidth) {
+  /**
+   * Constructs a Downscale function object for a given model's image(s) to the given new height and
+   * width.
+   *
+   * @param model     the model to get the images from to downscale.
+   * @param newHeight the new height to downscale the images to.
+   * @param newWidth  the new width to downscale the images to.
+   * @throws IllegalArgumentException if any arguments are null, or invalid.
+   */
+  public Downscale(IMultiLayerModel model, int newHeight, int newWidth)
+      throws IllegalArgumentException {
     if (model == null) {
       throw new IllegalArgumentException("Arguments are null");
+    }
+    if (newHeight <= 0 || newWidth <= 0) {
+      throw new IllegalArgumentException("Height and Width cannot be negative or zero");
+    }
+    int currentHeight = model.getImage().getHeight();
+    int currentWidth = model.getImage().getWidth();
+    if (newHeight >= currentHeight || newWidth >= currentWidth) {
+      throw new IllegalArgumentException("New Height/Width have to be smaller than original");
     }
     this.model = model;
     this.newHeight = newHeight;
     this.newWidth = newWidth;
   }
 
+  /**
+   * Applies a downscale operation to all layers in the multi-layer model.
+   */
   public void apply() {
     for (ILayer layer : this.model.getLayers()) {
       this.applyToLayer(layer);
     }
   }
 
+  /**
+   * Applies a downscale to a single layer.
+   *
+   * @param layer
+   */
   private void applyToLayer(ILayer layer) {
     IImage image = layer.getModel().getImage();
     IMatrix<IPixel> pixels = image.getPixelArray();
@@ -43,17 +69,19 @@ public class Downscale {
         this.newWidth);
     int originalWidth = image.getWidth();
     int originalHeight = image.getHeight();
-    for (int r = 0; r < pixels.getHeight(); r++) {
-      for (int c = 0; c < pixels.getWidth(); c++) {
-        double newX = (c / (double) originalWidth) * newWidth;
-        double newY = (r / (double) originalHeight) * newHeight;
-        if (newX % 1 == 0 || newY % 1 == 0) { // if newX and newY are ints
-          newPixels.updateEntry(pixels.getElement(r, c), (int) newY, (int) newX);
-        } else { // newX AND newY is floating point
-          int floorX = (int) Math.floor(newX);
-          int floorY = (int) Math.floor(newY);
-          int ceilX = (int) Math.ceil(newX);
-          int ceilY = (int) Math.ceil(newY);
+    for (int r = 0; r < newPixels.getHeight(); r++) {
+      for (int c = 0; c < newPixels.getWidth(); c++) {
+        double oldX = (c / (double) this.newWidth) * originalWidth;
+        double oldY = (r / (double) this.newHeight) * originalHeight;
+        if (oldX % 1 == 0 || oldY % 1 == 0) { // If either is an int
+          newPixels.updateEntry(pixels.getElement((int) oldY, (int) oldX), r, c);
+        } else {
+          // floor and ceiling of (x,y)
+          int floorX = (int) Math.floor(oldX);
+          int floorY = (int) Math.floor(oldY);
+          int ceilX = (int) Math.ceil(oldX);
+          int ceilY = (int) Math.ceil(oldY);
+          // Pixels ABCD:
           IPixel pixelA = pixels.getElement(floorY, floorX);
           IPixel pixelB = pixels.getElement(floorY, ceilX);
           IPixel pixelC = pixels.getElement(ceilY, floorX);
@@ -62,11 +90,11 @@ public class Downscale {
           int green = 0;
           int blue = 0;
           for (EChannelType channel : EChannelType.values()) { // for all color channels
-            double m = pixelB.getIntensity(channel) * (newX - floorX)
-                + pixelA.getIntensity(channel) * (ceilX - newX);
-            double n = pixelD.getIntensity(channel) * (newX - floorX)
-                + pixelC.getIntensity(channel) * (ceilX - newX);
-            double cp = n * (newY - floorY) + m * (ceilY - newY);
+            double m = pixelB.getIntensity(channel) * (oldX - floorX)
+                + pixelA.getIntensity(channel) * (ceilX - oldX);
+            double n = pixelD.getIntensity(channel) * (oldX - floorX)
+                + pixelC.getIntensity(channel) * (ceilX - oldX);
+            double cp = n * (oldY - floorY) + m * (ceilY - oldY);
             switch (channel) {
               case RED:
                 red = (int) cp;
@@ -80,11 +108,10 @@ public class Downscale {
             }
           }
           IPixel newPixel = new PixelImpl(red, green, blue);
-          newPixels.updateEntry(newPixel,);
+          newPixels.updateEntry(newPixel, r, c);
         }
       }
     }
-
   }
 
 }
