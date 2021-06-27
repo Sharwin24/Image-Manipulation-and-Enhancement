3dddb6aef5e70808722e9eb22b37d0f4e70224c6
amburke815 <burke.ad@northeastern.edu>
2021-06-27 16:44:36 -0500

    to sync changes

--- src/cs3500/view/GUIView.java
+++ src/cs3500/view/GUIView.java
@@ -98,7 +98,7 @@ public class GUIView extends JFrame implements IMEView, ActionListener, ItemList
       + "and-Enhancement.git";
 
   // to represent the model--the images to be manipulated
-  private IMultiLayerExtraOperations model = new MultiLayerModelImpl();
+  private final IMultiLayerExtraOperations model = new MultiLayerModelImpl();
 
 
   // to represent the scriptable controller embedded in the GUI
@@ -250,7 +250,6 @@ public class GUIView extends JFrame implements IMEView, ActionListener, ItemList
     setTitle("Image Manipulation and Enhancement");
     setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    this.model = new MultiLayerModelImpl();
     // to store interactively-scripted commands
     Readable scriptIn = new StringReader("");
     // to store output from the view
